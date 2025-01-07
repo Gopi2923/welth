d333d40a84bcc421eb58a929b4e051fb964432e4
Gopi2923 <dgopidhikshan@gmail.com>
2025-01-07 10:45:59 +0530

    worked on monthly budget feature.

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,31 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';
 import { getUserAccounts } from '@/actions/dashboard';
 import AccountCard from './_components/account-card';
+import { getCurrentBudget } from '@/actions/budget';
+import BudgetProgress from './_components/budget-progress';
 
 async function DashboardPage() {
 
   const accounts = await getUserAccounts()
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id)
+  }
+
+  console.log(budgetData);
   return (
-    <div className='px-5'>
+    <div className='space-y-8'>
 
           {/* Budget progress */}
+          {defaultAccount && (
+            <BudgetProgress 
+               initialBudget = {budgetData?.budget}
+               currentExpenses = {budgetData?.currentExpenses || 0}
+            />
+          )}
 
           {/* overview */}
 
